74f51664d60bf53ee3cac70e98924da2610270a5
A01039969 <A01039969@itesm.mx>
2021-10-26 12:45:15 -0500

    Adding Circle function

--- main.py
+++ main.py
@@ -1,25 +1,74 @@
 from turtle import *
 from freegames import vector
-    
-def store_color(iColor):
-    "Choose the color of the "
-    colour = state['store_color']
-    
-    if colour is None:
-        color('black','black')
-    else:
-        color(iColor, iColor)
 
-state = {'start': None, 'shape': line, 'store_color': 'blue'}
+def line(start, end):
+    "Draw line from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    goto(end.x, end.y)
+
+def square(start, end):
+    "Draw square from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+
+    end_fill()
+
+def circulo(start, end):
+    "Draw circle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    "Native function from Turtle which lets you make a circle, add the parameter which is the radius."
+    circle(50)
+    
+    end_fill()
+    
+    
+def rectangle(start, end):
+    pass
+
+def triangle(start, end):
+    pass
+    
+
+def tap(x, y):
+    "Store starting point or draw shape."
+    start = state['start']
+
+    if start is None:
+        state['start'] = vector(x, y)
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        shape(start, end)
+        state['start'] = None
+        
+def store(key, value):
+    "Store value in state at key."
+    state[key] = value
+    
+
+
+state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
 onkey(undo, 'u')
-onkey(lambda: store_color('black'), 'K')
-onkey(lambda: store_color('white'), 'W')
-onkey(lambda: store_color('green'), 'G')
-onkey(lambda: store_color('blue'), 'B')
-onkey(lambda: store_color('red'), 'R')
+onkey(lambda: color('black'), 'K')
+onkey(lambda: color('white'), 'W')
+onkey(lambda: color('green'), 'G')
+onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circulo), 'c')
